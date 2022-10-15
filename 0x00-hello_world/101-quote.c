823aadc2af28495e6abbcd369d7e2e46da185f89
bolu31 <iroghoisreal@gmail.com>
2022-10-15 13:45:10 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry
- * Return: 1
- */ 
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}
