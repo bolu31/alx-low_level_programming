3de8d6f71a5ff745851472daa63ddd44d43a8976
bolu31 <iroghoisreal@gmail.com>
2022-10-20 04:34:36 -0700

    my commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,21 +11,18 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-	if (a > b)
-	{
-		if (b > C)
-			largest = a;
-		else if (a > c)
-			largest = a;
-		else
-			largest = c;
-	}
-	else
-	{
-		if (b < C)
-			largest = c;
-		else
-			largest = b;
-	}
-	return (largest);
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+
+return (largest);
 }
