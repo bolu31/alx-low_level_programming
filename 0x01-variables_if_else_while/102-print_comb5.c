6966e88bc5d385419e3dce8fe4714d5e22172393
bolu31 <iroghoisreal@gmail.com>
2022-10-20 14:41:39 -0700

    my commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,43 +3,44 @@
 /**
  * main - Prints 3 combination of numbers
  *
- * Return: Always 0 (Success)
+ * Return: Always (Success)
  */
 int main(void)
 {
-		int c, i, k, j;
+	int c, i, k, j;
 
-		for (c = 48; c <= 57; c++)
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
 		{
-			for (i = 48; i <= 57; i++)
+			for (k = 48; k <= 57; k++)
 			{
-				for (k = 48; k <= 57; k++)
+				for (j = 48; j <= 57; j++)
 				{
-					for (j = 48; j <= 57; j++)
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
 					{
-						if (((k + j) > (c + i) &&  k >= c) || c < k)
-						{
-							putchar(c);
-							putchar(i);
-							putchar(' ');
-							putchar(k);
-							putchar(j);
+						putchar(c);
+						putchar(i);
+						putchar(' ');	
+						putchar(k);
+						putchar(j);
 
-						if (c + i + k + j == 227 && c == 57)
-						{
-						break;
-						}
-						else 
-						{
-						putchar(',');
-						putchar(' ');
-						}
-						}
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else 
+					{
+					putchar(',');
+					putchar(' ');
+					}
 					}
 				}
 			}
 		}
-		putchar('\n');
+	}
 
-		return (0);
+	putchar('\n');
+
+	return (0);
 }
