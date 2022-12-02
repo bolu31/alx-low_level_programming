0c3a28bfe908a6dc7752f251a1019aa2b0f26f51
bolu31 <iroghoisreal@gmail.com>
2022-12-02 14:46:59 -0800

    last task

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,16 +6,12 @@
  * @index: index position to change, starting from 0
  * Return: 1 if it worked, -1 if error
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int p;
-
-	if (index > 64)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	for (p = 1; index > 0; index--, p *= 2)
-		;
-	*n += p;
+	*n &= ~(1 << index);
 
 	return (1);
 }
